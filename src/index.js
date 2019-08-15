839596b626ab32c3dc5d85c9f3218c3426fab37b
dsych <dmytrosych834@gmail.com>
2019-08-15 12:32:57 -0400

    finished clock configuration

--- src/index.js
+++ src/index.js
@@ -13,12 +13,51 @@ const toggleSoundState = (parent, toBeEnabled, toBeDisabled) => {
     parent.querySelector(`div#${toBeEnabled}`).classList.remove("hidden");
 };
 
+const setupClock = parent => {
+    const minsEl = parent.querySelector(".digit > .mins");
+    const secsEl = parent.querySelector(".digit > .secs");
+
+    parent.querySelector(".minus").addEventListener("click", () => {
+        let mins = +minsEl.textContent;
+        let secs = +secsEl.textContent;
+
+        if (secs <= 0) {
+            if (mins > 0) {
+                mins--;
+                secs = 59;
+            }
+        } else {
+            secs--;
+        }
+        minsEl.textContent = String(mins).padStart(2, "0");
+        secsEl.textContent = String(secs).padStart(2, "0");
+    });
+
+    parent.querySelector(".plus").addEventListener("click", () => {
+        let mins = +minsEl.textContent;
+        let secs = +secsEl.textContent;
+
+        if (secs >= 59) {
+            if (mins < 59) {
+                mins++;
+                secs = 0;
+            }
+        } else {
+            secs++;
+        }
+        minsEl.textContent = String(mins).padStart(2, "0");
+        secsEl.textContent = String(secs).padStart(2, "0");
+    });
+};
+
 window.addEventListener("load", () => {
     const soundEl = document.querySelector("#sound");
     const mixerEl = document.querySelector("#mixer");
     const percentageEl = soundEl.querySelector("#percentage");
 
     const setsEl = document.querySelector("#sets");
+    const workEl = document.querySelector("#work");
+    const restEl = document.querySelector("#rest");
 
     // configure sound toggle
     soundEl.classList.add(soundOn);
@@ -61,8 +100,8 @@ window.addEventListener("load", () => {
         percentageEl.textContent = `${mixerEl.value} %`;
     });
 
-    const setsValueEl = setsEl.querySelector(".digit");
     // configure sets
+    const setsValueEl = setsEl.querySelector(".digit");
     setsEl.querySelector(".minus").addEventListener("click", () => {
         let v = parseInt(setsValueEl.textContent);
         if (v > 0) {
@@ -76,4 +115,10 @@ window.addEventListener("load", () => {
             setsValueEl.textContent = `${++v}`;
         }
     });
+
+    //configure work intervals
+    setupClock(workEl);
+
+    //configure rest intervals
+    setupClock(restEl);
 });
