f1d47cb155ac85f0aa5b5994420956dfd5874419
dsych <dmytrosych834@gmail.com>
2019-08-30 22:27:00 -0400

    adding start button functionality

---
+++ src/components/start-button.js
@@ -0,0 +1,56 @@
+class StartButton extends HTMLElement {
+    constructor() {
+        super();
+        this.buttonEl = null;
+        this.state = false;
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    static get observedAttributes() {
+        return ["paused"];
+    }
+
+    set paused(value) {
+        if (this.buttonEl) {
+            if (value === true) {
+                this.state = true;
+                this.buttonEl.classList.replace("fa-play", "fa-pause");
+            } else {
+                this.state = false;
+                this.buttonEl.classList.replace("fa-pause", "fa-play");
+            }
+            this.dispatchEvent(
+                new CustomEvent("start", { detail: { state: this.state } })
+            );
+        }
+    }
+
+    get paused() {
+        return this.state;
+    }
+
+    handleClick() {
+        this.paused = !this.state;
+    }
+
+    attributeChangedCallback(attrName, oldValue, newValue) {
+        this[attrName] = newValue;
+    }
+
+    init() {
+        this.buttonEl = this.querySelector(".play-button");
+        this.buttonEl.addEventListener("click", this.handleClick);
+        paused = this.state;
+    }
+
+    connectedCallback() {
+        const template = document.querySelector("#start");
+        this.appendChild(document.importNode(template.content, true));
+        this.init();
+    }
+    disconnectedCallback() {
+        this.buttonEl.removeEventListener("click", this.handleClick);
+    }
+}
+
+customElements.define("play-button", StartButton);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 "use strict";
-window.addEventListener("load", () => {});
+window.addEventListener("load", () => {
+    const playEl = document.querySelector("#play-button");
+    let countEl = document.querySelector("#count");
+    let workEl = document.querySelector("#work");
+    let restEl = document.querySelector("#rest");
+
+    const parent = document.querySelector("body > div");
+    playEl.addEventListener("start", e => {
+        if (e.detail.state === true) {
+            countEl.classList.add("hidden");
+            workEl.classList.add("hidden");
+            restEl.classList.add("hidden");
+        } else {
+            countEl.classList.remove("hidden");
+            workEl.classList.remove("hidden");
+            restEl.classList.remove("hidden");
+        }
+    });
+});
